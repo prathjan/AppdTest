40f3c81bc129358f2539b43a24ce61b5e517569b
prathjan <prathjan@cisco.com>
2021-09-02 19:14:48 -0700

    fix to jmeter

--- scripts/gentraffic.sh
+++ scripts/gentraffic.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 apt install openjdk-8-jdk -y
 cd /tmp
-tar xvf ./tmp/apache-jmeter-5.4.1.tgz 
+tar xvf apache-jmeter-5.4.1.tgz 
 cd apache-jmeter-5.4.1/bin
 java -jar ApacheJMeter.jar -t teastore_browse_nogui.jmx -Jhostname '$1' -Jport '$2' -JnumUser 10 -JrampUp 1 -l mylogfile.log -n
 
