314b4f8ab685befcc8b7ed62878edce61c210163
prathjan <prathjan@cisco.com>
2021-09-02 18:18:49 -0700

    set up dirs

--- scripts/gentraffic.sh
+++ scripts/gentraffic.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 apt install openjdk-8-jdk -y
-tar xvf ./scripts/apache-jmeter-5.4.1.tgz 
+cd /tmp
+tar xvf ./tmp/apache-jmeter-5.4.1.tgz 
 cd apache-jmeter-5.4.1/bin
 java -jar ApacheJMeter.jar -t teastore_browse_nogui.jmx -Jhostname '$1' -Jport '$2' -JnumUser 10 -JrampUp 1 -l mylogfile.log -n
 
