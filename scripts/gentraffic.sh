c3d73f0dafa188341adbb46d92beaeb41cf58760
prathjan <prathjan@cisco.com>
2021-09-02 21:21:31 -0700

    fixes to gent

--- scripts/gentraffic.sh
+++ scripts/gentraffic.sh
@@ -2,7 +2,9 @@
 apt install openjdk-8-jdk -y
 cd /tmp
 tar xvf apache-jmeter-5.4.1.tgz 
-cd apache-jmeter-5.4.1/bin
-cp /tmp/teastore_browse_nogui.jmx .
-java -jar ApacheJMeter.jar -t teastore_browse_nogui.jmx -Jhostname '$1' -Jport '$2' -JnumUser 10 -JrampUp 1 -l mylogfile.log -n
+#cd apache-jmeter-5.4.1/bin
+#cp /tmp/teastore_browse_nogui.jmx .
+java -jar ./apache-jmeter-5.4.1/bin/ApacheJMeter.jar -t teastore_browse_nogui.jmx -Jhostname 10.88.168.212 -Jport 8085 -JnumUser 10 -JrampUp 1 -l mylogfile.log -n
+
+
 
