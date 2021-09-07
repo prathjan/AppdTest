4605a6c02f3676568b4092032c42eec22f6a05a8
prathjan <prathjan@cisco.com>
2021-09-07 11:19:05 -0700

    update appvm

--- scripts/gentraffic.sh
+++ scripts/gentraffic.sh
@@ -4,7 +4,7 @@ cd /tmp
 tar xvf apache-jmeter-5.4.1.tgz 
 #cd apache-jmeter-5.4.1/bin
 #cp /tmp/teastore_browse_nogui.jmx .
-java -jar ./apache-jmeter-5.4.1/bin/ApacheJMeter.jar -t teastore_browse_nogui.jmx -Jhostname 10.88.168.212 -Jport 8085 -JnumUser 10 -JrampUp 1 -l mylogfile.log -n
+java -jar ./apache-jmeter-5.4.1/bin/ApacheJMeter.jar -t teastore_browse_nogui.jmx -Jhostname '$1' -Jport '$2' -JnumUser 10 -JrampUp 1 -l mylogfile.log -n
 
 
 
